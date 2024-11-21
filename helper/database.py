46e99413f6bc73a87ef60ce44ff46fc05fef8e7d
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-11-21 09:44:35 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -2,19 +2,24 @@ import motor.motor_asyncio
 from config import Config
 from .utils import send_log
 
+
 class Database:
 
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.madflixbotz = self._client[database_name]
-        self.col = self.madflixbotz.user
+        self.jishubotz = self._client[database_name]
+        self.col = self.jishubotz.user
 
     def new_user(self, id):
         return dict(
             _id=int(id),                                   
-            file_id=None,
-            caption=None,
-            format_template=None  # Add this line for the format template
+            file_id = None,
+            caption = None,
+            format_template=None,
+            prefix = None,
+            suffix = None,
+            metadata = False,
+            metadata_code = "By :- @Anime_warrior_Tamil"
         )
 
     async def add_user(self, b, m):
@@ -39,12 +44,20 @@ class Database:
     async def delete_user(self, user_id):
         await self.col.delete_many({'_id': int(user_id)})
     
+
+
+    #======================= Thumbnail ========================#
+
     async def set_thumbnail(self, id, file_id):
         await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
 
     async def get_thumbnail(self, id):
         user = await self.col.find_one({'_id': int(id)})
         return user.get('file_id', None)
+    
+    
+
+    #======================= Caption ========================#
 
     async def set_caption(self, id, caption):
         await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
@@ -52,6 +65,53 @@ class Database:
     async def get_caption(self, id):
         user = await self.col.find_one({'_id': int(id)})
         return user.get('caption', None)
+
+
+
+    #======================= Prefix ========================#
+
+    async def set_prefix(self, id, prefix):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'prefix': prefix}})  
+        
+    async def get_prefix(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('prefix', None)      
+    
+
+
+    #======================= Suffix ========================#
+
+    async def set_suffix(self, id, suffix):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'suffix': suffix}})  
+        
+    async def get_suffix(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('suffix', None)
+
+
+
+    #======================= Metadata ========================#
+        
+    async def set_metadata(self, id, bool_meta):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata': bool_meta}})
+        
+    async def get_metadata(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('metadata', None)
+        
+        
+        
+    #======================= Metadata Code ========================#    
+        
+    async def set_metadata_code(self, id, metadata_code):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata_code': metadata_code}})
+
+    async def get_metadata_code(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('metadata_code', None)  
+
+
+    #======================Auto rename code=======================#
 
     async def set_format_template(self, id, format_template):
         await self.col.update_one({'_id': int(id)}, {'$set': {'format_template': format_template}})
@@ -66,6 +126,7 @@ class Database:
     async def get_media_preference(self, id):
         user = await self.col.find_one({'_id': int(id)})
         return user.get('media_type', None)
+
 
 
 
